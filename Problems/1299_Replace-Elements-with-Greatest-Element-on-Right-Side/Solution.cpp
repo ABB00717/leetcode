3af2be56963cb6acd267b8ec04874850d880a6bd
ABB00717 <eric94528@gmail.com>
2024-07-11 11:14:28 +0800

    Leetcode: 1929

--- Problems/1299_Replace-Elements-with-Greatest-Element-on-Right-Side/Solution.cpp
+++ Problems/1299_Replace-Elements-with-Greatest-Element-on-Right-Side/Solution.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        vector<int> sortedArr = arr;
-        std::sort(sortedArr.begin(), sortedArr.end(), std::greater<int>());
-
-        vector<int> result;
-        for (int i = 0; i < arr.size(); i++) {
-            if (i != arr.size()-1 && arr[i] != sortedArr[i]) {
-                result.push_back(arr[i]);
-            } else if (i != arr.size()-1 && arr[i] == sortedArr[i]) {
-                arr.erase(arr.begin());
-                result.push_back(arr[i]);
-            } else {
-                result.push_back(-1);
-            }
+        int max = arr[arr.size()-1];
+        arr[arr.size()-1] = -1;
+        
+        for (int i = arr.size()-2; i >= 0; i--) {
+            int temp = arr[i];
+            arr[i] = max;
+            max = std::max(max, temp);
         }
 
-        return result;
+        return arr;
     }
 };
