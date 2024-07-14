0b29c23eedadf905c5b5699fb2fef8d5ac038879
ABB00717 <eric9452@gmail.com>
2024-07-14 11:55:41 +0800

    Leetcode: 27

---
+++ problems/27_Remove-Element/Solution.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int removeElement(vector<int> &nums, int val) {
+    int l = 0, r = nums.size();
+
+    while (l < r) {
+      if (nums[l] == val) {
+        nums[l] = nums[--r];
+      } else {
+        ++l;
+      }
+    }
+
+    return l;
+  }
+};
