2a4606f8232d1ca505ede1b944d986b9cab19937
ABB00717 <eric94528@gmail.com>
2024-08-05 12:04:26 +0800

    Neetcode: Binary-Search - 2

--- problems/74_Search-a-2D-Matrix/Solution.cpp
+++ problems/74_Search-a-2D-Matrix/Solution.cpp
@@ -7,9 +7,33 @@ public:
     int matrixColSize = matrix[0].size(), matrixRowSize = matrix.size();
 
     int row = 0;
+
+    int l = 0, r = matrixRowSize - 1;
+    int mid = (l + r) / 2;
+    while (matrix[std::max(0, mid - 1)][matrixColSize - 1] > target ||
+           matrix[std::min(matrixRowSize - 1, mid)][matrixColSize - 1] <
+               target) {
+    }
+
     for (row = 0;
-         row < matrixRowSize && matrix[row][matrixColSize - 1] < target;
+         row < matrixRowSize - 1 && matrix[row][matrixColSize - 1] < target;
          row++) {
     }
+
+    l = 0, r = matrixColSize - 1;
+    mid = (l + r) / 2;
+    while (matrix[row][mid] != target) {
+      if (matrix[row][l] > target || matrix[row][r] < target) {
+        return false;
+      } else if (matrix[row][mid] < target) {
+        l = mid + 1;
+      } else if (matrix[row][mid] > target) {
+        r = mid - 1;
+      }
+
+      mid = (l + r) / 2;
+    }
+
+    return true;
   }
 };
