d8819fbdf25822673e4392447164273ccf7e0d4f
ABB00717 <eric94528@gmail.com>
2024-09-05 22:43:47 +0800

    Daily: 2024/09/05

--- problems/2028_Find-Missing-Observations/Solution.cpp
+++ problems/2028_Find-Missing-Observations/Solution.cpp
@@ -1,25 +1,23 @@
-  #include <algorithm>
-  #include <numeric>
-  #include <vector>
-  class Solution {
-  public:
-    std::vector<int> missingRolls(std::vector<int> &rolls, int mean, int n) {
-      std::vector<int> result(n, 0);
+#include <numeric>
+#include <vector>
+class Solution {
+public:
+  std::vector<int> missingRolls(std::vector<int> &rolls, int mean, int n) {
+    int rollsAccumulate = std::accumulate(rolls.begin(), rolls.end(), 0);
+    int totalRoll = rolls.size() + n;
+    int target = mean * totalRoll;
 
-      int rollsAccumulate = std::accumulate(rolls.begin(), rolls.end(), 0);
-      int totalRoll = rolls.size() + n;
-      int target = mean * totalRoll;
+    if (rollsAccumulate + n > target || rollsAccumulate + n * 6 < target)
+      return {};
 
-      if (rollsAccumulate + n > target || rollsAccumulate + n * 6 < target) return {};
+    int remaining = target - rollsAccumulate;
+    std::vector<int> result(n, remaining / n);
+    int remainder = remaining % n;
 
-      int remaining = target - rollsAccumulate;
-
-      for (int i = 0; i < n; i++) {
-        int roll = std::max(1, static_cast<int>((remaining) / (n - i)));
-        result[i] = roll;
-        remaining -= roll;
-      }
-
-      return result;
+    for (int i = 0; i < remainder; i++) {
+      result[i]++;
     }
-  };
+
+    return result;
+  }
+};
