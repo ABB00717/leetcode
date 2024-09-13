a8ceeca1f2ba6012ecc327fc265bda4d2ab2aa63
ABB00717 <eric94528@gmail.com>
2024-09-13 10:59:59 +0800

    Daily: 2024/09/13

--- problems/1310_XOR-Queries-of-a-Subarray/Solution.cpp
+++ problems/1310_XOR-Queries-of-a-Subarray/Solution.cpp
@@ -3,12 +3,13 @@
 class Solution {
 public:
   std::vector<int> xorQueries(std::vector<int> &arr, std::vector<std::vector<int>> &queries) {
-    std::vector<int> result(queries.size(), 0);
+    std::vector<int> result(queries.size(), 0), prefixXor(arr.size() + 1, 0);
+    for (int i = 1; i <= arr.size(); i++) {
+      prefixXor[i] = prefixXor[i - 1] ^ arr[i - 1];
+    }
 
     for (int i = 0; i < queries.size(); i++) {
-      for (int j = queries[i][0]; j <= queries[i][1]; j++) {
-        result[i] ^= arr[j];
-      }
+      result[i] = prefixXor[queries[i][0]] ^ prefixXor[queries[i][1]];
     }
 
     return result;
