40a28dea5f6146db1bf63e45df57c2658b536acb
ABB00717 <eric94528@gmail.com>
2024-09-19 14:57:42 +0800

    Daily: 2024/09/19

--- problems/241_Different-Ways-to-Add-Parentheses/Solution.cpp
+++ problems/241_Different-Ways-to-Add-Parentheses/Solution.cpp
@@ -1,27 +1,17 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
 
 class Solution {
 public:
   std::vector<int> diffWaysToCompute(std::string expression) {
-    return helper(expression);
-  }
-  
-private:
-  std::unordered_map<std::string, std::vector<int>> memo;
-  
-  std::vector<int> helper(const std::string& expression) {
-    if (memo.find(expression) != memo.end()) {
-      return memo[expression];
-    }
-    
     std::vector<int> result;
+
     for (int i = 0; i < expression.size(); i++) {
       char c = expression[i];
       if (c == '+' || c == '-' || c == '*') {
-        std::vector<int> left = helper(expression.substr(0, i));
-        std::vector<int> right = helper(expression.substr(i + 1));
+        std::vector<int> left = diffWaysToCompute(expression.substr(0, i));
+        std::vector<int> right = diffWaysToCompute(expression.substr(i + 1));
+
         for (int l : left) {
           for (int r : right) {
             if (c == '+') {
@@ -35,10 +25,7 @@ private:
         }
       }
     }
-    if (result.empty()) {
-      result.push_back(std::stoi(expression));
-    }
-    memo[expression] = result;
-    return result;
+
+    return result.empty() ? std::vector<int>{std::stoi(expression)} : result;
   }
 };
