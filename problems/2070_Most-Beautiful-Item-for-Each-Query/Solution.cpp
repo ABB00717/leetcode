6f9bd85ac66e56506128d0d7794a3a8f36530606
ABB00717 <eric94528@gmail.com>
2024-11-12 12:37:27 +0800

    Daily: 2024/11/12

--- problems/2070_Most-Beautiful-Item-for-Each-Query/Solution.cpp
+++ problems/2070_Most-Beautiful-Item-for-Each-Query/Solution.cpp
@@ -1,42 +1,16 @@
-#include <random>
-#include <utility>
-#include <vector>
 #include <algorithm>
+#include <cstdlib>
+#include <vector>
 
 class Solution {
-private:
-  std::mt19937 rng;
-
-  int randomRange(int left, int right) {
-    std::uniform_int_distribution<std::mt19937::result_type> randIndex(left, right);
-    return randIndex(rng);
-  }
-
-  void quickSort(std::vector<std::vector<int>>& items, int left, int right) {
-    if (left >= right) return;
-
-    std::swap(items[left], items[randomRange(left, right)]);
-    int middle = left;
-
-    for (int i = left + 1; i <= right; i++) {
-      if (items[i][0] < items[left][0] || (items[i][0] == items[left][0] && items[i][1] > items[left][1])) {
-        std::swap(items[++middle], items[i]);
-      }
-    }
-
-    std::swap(items[left], items[middle]);
-    quickSort(items, left, middle - 1);
-    quickSort(items, middle + 1, right);
-  }
-  
 public:
-  Solution() : rng(std::random_device{}()) {}
-
   std::vector<int> maximumBeauty(std::vector<std::vector<int>> &items, std::vector<int> &queries) {
-    quickSort(items, 0, items.size() - 1);
+    std::sort(items.begin(), items.end(), [](const std::vector<int>& a, const std::vector<int>& b){
+      return a[0] < b[0] || a[0] == b[0] && a[1] < b[1];
+    });
 
     int max = 0;
-    for (auto& item : items) {
+    for (auto &item : items) {
       if (item[1] > max) max = item[1];
       item[1] = max;
     }
@@ -48,7 +22,7 @@ public:
 
       while (left <= right) {
         int middle = left + (right - left) / 2;
-        if (items[middle][0] <= query) { 
+        if (items[middle][0] <= query) {
           bestBeauty = items[middle][1];
           left = middle + 1;
         } else {
