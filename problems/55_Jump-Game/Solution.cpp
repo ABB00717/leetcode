21c57070996cfafdc6baa28c628ea3ab9243ac41
ABB00717 <eric94528@gmail.com>
2024-11-19 15:30:44 +0800

    Leetcode: 55

---
+++ problems/55_Jump-Game/Solution.cpp
@@ -0,0 +1,17 @@
+#include <queue>
+#include <set>
+#include <vector>
+class Solution {
+public:
+  bool canJump(std::vector<int> &nums) {
+    int farthest = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+      if (i > farthest) return false;
+      farthest = std::max(farthest, i + nums[i]);
+      if (farthest >= nums.size() - 1) return true;
+    }
+
+    return false;
+  }
+};
