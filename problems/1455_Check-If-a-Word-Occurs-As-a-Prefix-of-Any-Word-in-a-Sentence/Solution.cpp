ce98b034b2839c066876eaa14464e4635fe12136
ABB00717 <eric94528@gmail.com>
2024-12-02 11:33:20 +0800

    Daily: 2024/12/02

--- problems/1455_Check-If-a-Word-Occurs-As-a-Prefix-of-Any-Word-in-a-Sentence/Solution.cpp
+++ problems/1455_Check-If-a-Word-Occurs-As-a-Prefix-of-Any-Word-in-a-Sentence/Solution.cpp
@@ -1,36 +1,21 @@
 #include <cstring>
+#include <sstream>
 #include <string>
-#include <vector>
 
 class Solution {
-private:
-  std::vector<std::string> split(std::string str, char delim) {
-    std::vector<std::string> words;
-    std::string word = "";
-    for (int i = 0; i < str.size(); i++) {
-      if (str[i] == delim) {
-        words.push_back(word);
-        word = "";
-      } else {
-        word += str[i];
-      }
-    }
-    words.push_back(word);
-    return words;
-  }
-  
 public:
   int isPrefixOfWord(std::string sentence, std::string searchWord) {
-    int result = -1;    
-    std::vector<std::string> words = split(sentence, ' ');
+    std::istringstream iss(sentence);
+    std::string word;
+    int index = 1;
 
-    for (int i = 0; i < words.size(); i++) {
-      if (words[i].find(searchWord) == 0) {
-        result = i + 1;
-        break;
+    while (iss >> word) {
+      if (word.compare(0, searchWord.size(), searchWord) == 0) {
+        return index;
       }
+      index++;
     }
 
-    return result;
+    return -1;
   }
 };
