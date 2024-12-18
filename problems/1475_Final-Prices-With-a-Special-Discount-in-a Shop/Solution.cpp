122436852bd8be1d637f737af249457d7fe8d62f
ABB00717 <eric94528@gmail.com>
2024-12-18 10:59:32 +0800

    Daily: 2024/12/18

--- problems/1475_Final-Prices-With-a-Special-Discount-in-a Shop/Solution.cpp
+++ problems/1475_Final-Prices-With-a-Special-Discount-in-a Shop/Solution.cpp
@@ -1,11 +1,20 @@
+#include <stack>
 #include <vector>
 class Solution {
+/*
+遍歷 prices，在這過程中持續把每個 price 的 index 丟進 stack，假設當前的 price 小於 stack 上的 price，
+則透過 index 更改那個 price 值，並且持續這麼做直到不符合條件，之後才繼續遍歷 price。
+*/
 public:
   std::vector<int> finalPrices(std::vector<int> &prices) {
+    std::stack<int> stack;
+
     for (int i = 0; i < prices.size(); i++) {
-      int j = i+1;
-      for (j = i+1; j < prices.size() && prices[j] > prices[i]; j++) {}
-      if (j != prices.size()) prices[i] -= prices[j];
+      while (!stack.empty() && prices[stack.top()] >= prices[i]) {
+        prices[stack.top()] -= prices[i];
+        stack.pop();
+      }
+      stack.push(i);
     }
 
     return prices;
