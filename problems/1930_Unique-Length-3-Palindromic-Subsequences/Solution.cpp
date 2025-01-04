f03af7845fa18839fe2ccdbbfaf896a5aa2711e1
ABB00717 <eric94528@gmail.com>
2025-01-04 09:56:38 +0800

    Daily: 2025/01/04

--- problems/1930_Unique-Length-3-Palindromic-Subsequences/Solution.cpp
+++ problems/1930_Unique-Length-3-Palindromic-Subsequences/Solution.cpp
@@ -1,36 +1,39 @@
-#include <iostream>
-#include <ostream>
-#include <set>
+#include <cstdlib>
 #include <string>
+#include <vector>
 class Solution {
 public:
+  /*因為只要求三個字元的 Palindrom，所以只要檢測 prefix == suffix 以及中間某個字元有沒有出現即可*/
   int countPalindromicSubsequence(std::string s) {
-    std::set<std::string> result;
+    if (s.size() < 3)
+      return 0;
 
-    int l = 0;
-    std::set<char> usedPrefix;
-    while (l < s.size()) {
-      usedPrefix.insert(s[l]);
-      int r = s.size() - 1;
-      while (l < r) {
-        if (s[l] != s[r]) {
-          r--;
-          continue;
-        }
-
-        for (int i = l+1; i < r; i++) {
-          std::string palindrom;
-          palindrom += s[l];
-          palindrom += s[i];
-          palindrom += s[r];
-          result.insert(palindrom);
-        }
-        break;
+    std::vector<std::vector<int>> prefixCount(
+        26, std::vector<int>(s.size() + 1, 0));
+    prefixCount[s[0] - 'a'][0] = 1;
+    for (int i = 1; i < s.size(); i++) {
+      for (char target = 'a'; target <= 'z'; target++) {
+        prefixCount[target - 'a'][i] = prefixCount[target - 'a'][i - 1];
+        if (s[i] == target)
+          prefixCount[target - 'a'][i]++;
       }
-      while (usedPrefix.find(s[l]) != usedPrefix.end())
-        l++;
     }
 
-    return result.size();
+    std::vector<std::vector<int>> positions(26);
+    for (int i = 0; i < s.size(); i++) {
+      positions[s[i]-'a'].push_back(i);
+    }
+
+    int result = 0;
+    for (char prefix = 0; prefix < 26; prefix++) {
+      if (positions[prefix].size() < 2) continue;
+      int l = positions[prefix][0], r = positions[prefix][positions[prefix].size()-1];
+
+      for (char mid = 0; mid < 26; mid++) {
+        if (prefixCount[mid][r-1] - prefixCount[mid][l] != 0) result++;
+      }
+    }
+
+    return result;
   }
 };
