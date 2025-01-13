1d5cab21383781fd2f177ebb641061d4475016a3
ABB00717 <eric94528@gmail.com>
2025-01-14 00:17:05 +0800

    Daily: 2025/01/14

--- problems/3223_Minimum-Length-of-String-After-Operations/Solution.cpp
+++ problems/3223_Minimum-Length-of-String-After-Operations/Solution.cpp
@@ -5,35 +5,24 @@
 class Solution {
 public:
   int minimumLength(std::string s) {
-    std::vector<char> resultString(s.size(), '-');
+    // 5 -> 3
+    // 6 -> 2 3
 
-    std::vector<std::deque<int>> charCount(26, std::deque<int>());
-    for (int i = 0; i < s.size(); i++)
-      charCount[s[i] - 'a'].push_back(i);
+    std::vector<int> charFreq(26, 0);
+    for (const char& c : s) 
+      charFreq[c - 'a']++;
 
-    for (auto &charDeque : charCount) {
-      while (charDeque.size() > 2) {
-        charDeque.pop_front();
-        charDeque.pop_back();
-      }
-    }
-
-    for (int i = 0; i < 26; i++) {
-      if (charCount[i].empty())
+    int result = 0;
+    for (int& freq : charFreq) {
+      if (freq == 0)
         continue;
-      
-      if (charCount[i].size() >= 1)
-        resultString[charCount[i].front()] = i + 'a';
-      if (charCount[i].size() > 1)
-        resultString[charCount[i].back()] = i + 'a';
+
+      if (freq % 2 == 1)
+        result += 1;
+      else
+        result += 2;
     }
 
-    std::string result;
-    for (auto &c : resultString) {
-      if (c != '-')
-        result += c;
-    }
-
-    return result.size();
+    return result;
   }
 };
