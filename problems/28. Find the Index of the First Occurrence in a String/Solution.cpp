2ac5b95e9b7b6229cd14550fea5cdd9835edb070
ABB00717 <eric94528@gmail.com>
2025-02-24 17:58:46 +0800

    Top 150: 28

--- problems/28. Find the Index of the First Occurrence in a String/Solution.cpp
+++ problems/28. Find the Index of the First Occurrence in a String/Solution.cpp
@@ -1,7 +1,43 @@
 #include <string>
+#include <vector>
+
+// LPS 但我其實搞不太懂耶
 class Solution {
+private:
+  std::vector<int> computeLPS(std::string needle) {
+    int n = needle.size(), len = 0;
+    std::vector<int> lps(n, 0);
+
+    for (int i = 1; i < n; ) {
+      if (needle[i] == needle[len]) {
+        lps[i++] = ++len;
+      } else if (len) {
+        len = lps[len - 1];
+      } else {
+        lps[i++] = 0;
+      }
+    }
+
+    return lps;
+  }
+  
 public:
   int strStr(std::string haystack, std::string needle) {
-    return haystack.find(needle);
+    if (needle.empty()) return 0;
+    std::vector<int> lps = computeLPS(needle);
+
+    int i = 0, j = 0, m = haystack.size(), n = needle.size();
+    while (i < m) {
+      if (haystack[i] == needle[j]) {
+        i++, j++;
+        if (j == n) return i - n;
+      } else if (j) {
+        j = lps[j - 1];
+      } else {
+        i++;
+      }
+    }
+
+    return -1;
   }
 };
