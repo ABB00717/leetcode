de9d5e4ac7b449ae8316bd32af2874b8f619ead9
ABB00717 <eric94528@gmail.com>
2025-02-25 09:27:38 +0800

    Top 150: 151

---
+++ problems/151. Reverse Words in a String/Solution.cpp
@@ -0,0 +1,19 @@
+#include <sstream>
+#include <string>
+class Solution {
+public:
+  std::string reverseWords(std::string s) {
+    std::stringstream ss(s);
+    
+    std::string word, reversedWords;
+
+    while (ss >> word) {
+      reversedWords.insert(0, " " + word);
+    }
+
+    if (reversedWords.empty()) return "";
+    reversedWords.erase(0, 1);
+
+    return reversedWords;
+  }
+};
