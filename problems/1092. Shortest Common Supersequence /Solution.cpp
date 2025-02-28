fd00c873ed128c230bd18c35cabda12940e6190d
ABB00717 <eric94528@gmail.com>
2025-02-28 09:49:42 +0800

    Daily: 2025/02/28
    因為直接儲存字串的話會浪費大量記憶體空間，所以轉而儲存長度。最後再回朔，檢查當前的長度和哪個鄰近的dp有差，就代表是從那個字串傳下來的。

--- problems/1092. Shortest Common Supersequence /Solution.cpp
+++ problems/1092. Shortest Common Supersequence /Solution.cpp
@@ -1,33 +1,52 @@
-#include <string>
-#include <unordered_map>
-#include <vector>
-class Solution {
-public:
-  std::string shortestCommonSupersequence(std::string str1, std::string str2) {
-    int m = str1.size(), n = str2.size();
-    std::vector<std::vector<std::string>> dp(m+1, std::vector<std::string>(n+1, ""));
-    
-    // init dp
-    for (int i = 0; i <= m; ++i) {
-      for (int j = 0; j <= n; ++j) {
-        if (i == 0) {
-          dp[i][j] = str2.substr(0, j);
-        } else if (j == 0) {
-          dp[i][j] = str1.substr(0, i);
-        } else if (str1[i - 1] == str2[j - 1]) {
-          dp[i][j] = dp[i - 1][j - 1] + str1[i - 1];
-        } else {
-          if (dp[i - 1][j].size() < dp[i][j - 1].size()) {
-            dp[i][j] = dp[i - 1][j] + str1[i - 1];
-          } else if (dp[i - 1][j].size() > dp[i][j - 1].size()) {
-            dp[i][j] = dp[i][j - 1] + str2[j - 1];
+  #include <string>
+  #include <vector>
+  class Solution {
+  public:
+    std::string shortestCommonSupersequence(std::string str1, std::string str2) {
+      int m = str1.size(), n = str2.size();
+      std::vector<std::vector<int>> dp(m+1, std::vector<int>(n+1, 0));
+      
+      // init dp
+      for (int i = 0; i <= m; ++i) {
+        for (int j = 0; j <= n; ++j) {
+          if (i == 0) {
+            dp[i][j] = j;
+          } else if (j == 0) {
+            dp[i][j] = i;
+          } else if (str1[i - 1] == str2[j - 1]) {
+            dp[i][j] = dp[i - 1][j - 1] + 1;
           } else {
-            dp[i][j] = (dp[i - 1][j] + str1[i - 1] < dp[i][j - 1] + str2[j - 1]) ? (dp[i - 1][j] + str1[i - 1]) : (dp[i][j - 1] + str2[j - 1]);
+            dp[i][j] = std::min(dp[i - 1][j], dp[i][j - 1]) + 1;
           }
         }
       }
-    }
 
-    return dp[m][n];
-  }
-};
+      // 回朔構造
+      std::string result = "";
+      int i = m, j = n;
+      while (i > 0 && j > 0) {
+        if (str1[i - 1] == str2[j - 1]) {
+          result += str1[i - 1];
+          --i;
+          --j;
+        } else if (dp[i - 1][j] < dp[i][j - 1]) {
+          result += str1[i - 1];
+          --i;
+        } else {
+          result += str2[j - 1];
+          --j;
+        }
+      }
+
+      while (i > 0) {
+        result += str1[i - 1];
+        --i;
+      }
+      while (j > 0) {
+        result += str2[j - 1];
+        --j;
+      }
+
+      return std::string(result.rbegin(), result.rend());
+    }
+  };
