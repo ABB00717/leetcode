c0e28af68098a6e9afcaea6302a26b0bb91f7133
ABB00717 <eric94528@gmail.com>
2025-03-02 13:13:33 +0800

    Leetcode 75: 283
    因為非零的一定會在前面，所以我們從0開始追蹤。
    假設在遍歷過程中遇到不是零的，那麼就交換，直到結束。

--- problems/283. Move Zeroes/Solution.cpp
+++ problems/283. Move Zeroes/Solution.cpp
@@ -1,25 +1,14 @@
+#include <utility>
 #include <vector>
 class Solution {
 public:
   void moveZeroes(std::vector<int> &nums) {
-    int countZero = 0;
-    for (int num : nums) {
-      if (num == 0)
-        countZero++;
-    }
+    int nonZeroIndex = 0;
 
-    for (int i = nums.size()-1; i >= 0 && nums[i] == 0; i--) {
-      countZero--;
-    }
-
-    int i = 0;
-    while (countZero > 0) {
-      if (nums[i] == 0) {
-        nums.erase(nums.begin() + i);
-        nums.push_back(0);
-        countZero--;
-      } else {
-        i++;
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] != 0) {
+        std::swap(nums[i], nums[nonZeroIndex]);
+        nonZeroIndex++;
       }
     }
   }
