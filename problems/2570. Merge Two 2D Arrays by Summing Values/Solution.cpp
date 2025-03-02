b613b7e4d60215a12cee58dbb9af0024ef2ab7a6
ABB00717 <eric94528@gmail.com>
2025-03-02 12:45:39 +0800

    Daily: 2025/03/02

--- problems/2570. Merge Two 2D Arrays by Summing Values/Solution.cpp
+++ problems/2570. Merge Two 2D Arrays by Summing Values/Solution.cpp
@@ -12,28 +12,19 @@ public:
     int i1 = 0, i2 = 0;
     while (i1 < n1 && i2 < n2) {
       if (nums1[i1][0] == nums2[i2][0]) {
-        result.push_back({nums1[i1][0], nums1[i1][1] + nums2[i2][1]});
-        i1++;
-        i2++;
+        result.push_back({nums1[i1][0], nums1[i1++][1] + nums2[i2++][1]});
       } else if (nums1[i1][0] > nums2[i2][0]) {
-        result.push_back(nums2[i2]);
-        i2++;
+        result.push_back(nums2[i2++]);
       } else {
-        result.push_back(nums1[i1]);
-        i1++;
+        result.push_back(nums1[i1++]);
       }
     }
 
-    if (i1 == n1) {
-      while (i2 < n2) {
-        result.push_back(nums2[i2]);
-        i2++;
-      }
-    } else if (i2 == n2) {
-      while (i1 < n1) {
-        result.push_back(nums1[i1]);
-        i1++;
-      }
+    while (i2 < n2) {
+      result.push_back(nums2[i2++]);
+    }
+    while (i1 < n1) {
+      result.push_back(nums1[i1++]);
     }
 
     return result;
