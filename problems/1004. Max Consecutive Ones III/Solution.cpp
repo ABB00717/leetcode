0107799c1864b48f83ee701a3dc2839aa9b045e3
ABB00717 <eric94528@gmail.com>
2025-03-02 14:43:13 +0800

    Leetcode 75: 1004
    反正我就一直往右啊，不符合條件是你的問題，是你要動不是我要動欸。

--- problems/1004. Max Consecutive Ones III/Solution.cpp
+++ problems/1004. Max Consecutive Ones III/Solution.cpp
@@ -3,27 +3,23 @@
 class Solution {
 public:
   int longestOnes(std::vector<int> &nums, int k) {
-    int l = 0, r = 0, zeroNum = 0;
+    int l = 0, zeroCount = 0;
     int maxLen = 0;
 
-    while (r < nums.size()) {
-      if (nums[r] == 0)
-        zeroNum++;
+    for (int r = 0; r < nums.size(); r++) {
+      if (nums[r] == 0) {
+        zeroCount++;
+      }
 
-      if (zeroNum <= k) {
-        r++;
-      } else if (zeroNum > k) {
-        maxLen = std::max(maxLen, r - l);
-        while (nums[l] == 1 && l < nums.size()) {
-          l++;
+      while (zeroCount > k) {
+        if (nums[l] == 0) {
+          zeroCount--;
         }
         l++;
-        r++;
-        zeroNum--;
       }
-    } 
 
-    maxLen = std::max(maxLen, r - l);
+      maxLen = std::max(maxLen, r - l + 1);
+    }
 
     return maxLen;
   }
