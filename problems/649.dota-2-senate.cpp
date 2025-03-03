c8a2b9f70a93c37142c0cccd95d436eb653e1dc6
ABB00717 <eric94528@gmail.com>
2025-03-03 11:56:09 +0800

    Leetcode 75: 649
    直接把前面的index + n，就不用再維護prevIndex了。

--- problems/649.dota-2-senate.cpp
+++ problems/649.dota-2-senate.cpp
@@ -5,56 +5,37 @@
  */
 
 // @lc code=start
-#include <algorithm>
-#include <deque>
+#include <queue>
 #include <string>
 class Solution {
-private:
-  void banOpponent(std::deque<int>& exchange, std::deque<int>& pop, int& i) {
-    i = exchange.front();
-    exchange.push_back(i);
-    exchange.pop_front();
-    pop.pop_front();
-  }
-
 public:
   std::string predictPartyVictory(std::string senate) {
-    std::deque<int> dDeque, rDeque;
+    std::queue<int> rQueue, dQueue;
+    int n = senate.size();
 
-    for (int i = 0; i < senate.size(); i++) {
+    for (int i = 0; i < n; i++) {
       if (senate[i] == 'R') {
-        rDeque.push_back(i);
-      } else if (senate[i] == 'D') {
-        dDeque.push_back(i);
-      }
-    }
-
-    int lastIndex = -1;
-    while (!dDeque.empty() && !rDeque.empty()) {
-      if (lastIndex >= dDeque.front() && lastIndex >= rDeque.front()) {
-        lastIndex = std::min(dDeque.front(), rDeque.front());
-
-        if (lastIndex == dDeque.front()) {
-          dDeque.push_back(lastIndex);
-        } else {
-          rDeque.push_back(lastIndex);
-        }
-        rDeque.pop_front();
-        dDeque.pop_front();
-      } else if (lastIndex >= dDeque.front()) {
-        banOpponent(rDeque, dDeque, lastIndex);
-      } else if (lastIndex >= rDeque.front()) {
-        banOpponent(dDeque, rDeque, lastIndex);
+        rQueue.push(i);
       } else {
-        if (dDeque.front() > rDeque.front()) {
-          banOpponent(rDeque, dDeque, lastIndex);
-        } else {
-          banOpponent(dDeque, rDeque, lastIndex);
-        }
+        dQueue.push(i);
       }
     }
 
-    return dDeque.empty() ? "Radiant" : "Dire";
+    while (!rQueue.empty() && !dQueue.empty()) {
+      int rIndex = rQueue.front();
+      int dIndex = dQueue.front();
+      rQueue.pop();
+      dQueue.pop();
+
+      if (rIndex < dIndex) {
+        rQueue.push(rIndex + n);
+      } else {
+        dQueue.push(dIndex + n);
+      }
+
+    }
+
+    return rQueue.empty() ? "Dire" : "Radiant";
   }
 };
 // @lc code=end
