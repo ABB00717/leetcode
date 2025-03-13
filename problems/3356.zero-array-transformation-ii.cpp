0d0505ae474137a20535cf746b21fd7fb9b8b5cb
ABB00717 <eric94528@gmail.com>
2025-03-13 17:01:31 +0800

    Daily: 2025/03/13
    模組化程式碼

--- problems/3356.zero-array-transformation-ii.cpp
+++ problems/3356.zero-array-transformation-ii.cpp
@@ -7,56 +7,62 @@
 // @lc code=start
 #include <algorithm>
 #include <cstdlib>
-#include <iostream>
-#include <ostream>
-#include <utility>
 #include <vector>
 class Solution {
 public:
-  int minZeroArray(std::vector<int> &nums, std::vector<std::vector<int>> &queries) {
-    std::vector<int> prefixSum(nums.size()+1, 0);
-    for (const auto& query : queries) {
-      prefixSum[query[0]] += query[2];
-      prefixSum[query[1]+1] -= query[2];
-    }
+  int minZeroArray(std::vector<int> &nums,
+                   std::vector<std::vector<int>> &queries) {
+    std::vector<int> diffs(nums.size() + 1, 0);
+    accumulateToK(queries, diffs, queries.size() - 1);
 
-    int cur = 0;
-    bool allZero = true;
-    for (int i = 0; i < nums.size(); i++) {
-      cur += prefixSum[i];
-      if (nums[i] - cur > 0)
-        return -1;
-      if (nums[i] != 0)
-        allZero = false;
-    }
-
-    if (allZero)
+    if (!isValid(nums, diffs))
+      return -1;
+    if (isAllZero(nums))
       return 0;
 
-    int l = 0, h = queries.size()-1;
-    while (l < h) {
-      int k = (l + h) / 2;
-      prefixSum.assign(nums.size()+1, 0);
+    int l = 0, h = queries.size() - 1;
+    int answer = -1;
 
-      for (int i = 0; i <= k; i++) {
-        prefixSum[queries[i][0]] += queries[i][2];
-        prefixSum[queries[i][1]+1] -= queries[i][2];
+    while (l <= h) {
+      int k = l + (h - l) / 2;
+
+      diffs.assign(nums.size() + 1, 0);
+      accumulateToK(queries, diffs, k);
+
+      if (isValid(nums, diffs)) {
+        h = k - 1;
+        answer = k + 1;
+      } else {
+        l = k + 1;
       }
-      
-      int i = 0, cur = 0;
-      for (i = 0; i < nums.size(); i++) {
-        cur += prefixSum[i];
-        if (nums[i] - cur > 0) {
-          l = k + 1;
-          break;
-        }
-      }
-      
-      if (i == nums.size())
-        h = k;
     }
-    
-    return l == queries.size() ? -1 : l + 1;
+
+    return answer;
+  }
+
+private:
+  void accumulateToK(const std::vector<std::vector<int>> &queries,
+                     std::vector<int> &diffs, int k) {
+    for (int i = 0; i <= k; i++) {
+      diffs[queries[i][0]] += queries[i][2];
+      diffs[queries[i][1] + 1] -= queries[i][2];
+    }
+  }
+
+  bool isValid(const std::vector<int> &nums, const std::vector<int> &diffs) {
+    int cur = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      cur += diffs[i];
+      if (nums[i] - cur > 0)
+        return false;
+    }
+
+    return true;
+  }
+
+  bool isAllZero(const std::vector<int> &nums) {
+    return std::all_of(nums.begin(), nums.end(),
+                       [](int num) { return num == 0; });
   }
 };
 // @lc code=end
