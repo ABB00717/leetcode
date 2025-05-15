0781f61e4fec60410fb849afef1a5d984d25d07f
ABB00717 <abb00717@gmail.com>
2025-05-15 11:43:11 +0800

    Daily: solve Leetcode 2900 using Logic

---
+++ problems/python/2900.longest-unequal-adjacent-groups-subsequence-i.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2900 lang=python3
+#
+# [2900] Longest Unequal Adjacent Groups Subsequence I
+#
+
+# @lc code=start
+class Solution:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        
+# @lc code=end
+
